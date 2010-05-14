562dc8a60f8cac891902197ebf03aa33178099cf
Lena <vyazovetskova@yandex.ru>
2010-05-14 22:59:01 +0400

    Была ошибка в description.rb(вместо catalog ->catalod). Эта миграция исправить ситуацию)

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -1,5 +1,5 @@
 class Catalog < ActiveRecord::Base
-  has_one :discription
+  has_one :description
   validates_presence_of :art, :name
   validates_length_of :art, :within => 1..10, :message =>"Данные введены неверно!"
   validates_length_of :name, :within => 1..50, :message =>"Данные введены неверно!"
--- app/models/description.rb
+++ app/models/description.rb
@@ -3,11 +3,12 @@ class Description < ActiveRecord::Base
   validates_presence_of :dlina, :color, :shin, :cost, :fulart
   validates_length_of :color, :within => 1..15, :message =>"Данные введены неверно!"
   def validate
-    if Description.dlina<= 0.0
+    if self.dlina<= 0.0
       errors.add ( "Данные введены не верно!")
     end
-      if Description.cost<= 0.0
+      if self.cost<= 0.0
       errors.add ( "Данные введены не верно!")
     end
   end
 end
+
