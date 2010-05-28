7ab19ed0eac901d5dc6106b4b55bb5b5a3fd1bc5
Ilia <formal7@yandex.ru>
2010-05-28 11:04:14 +0400

    kzkzkz

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -4,5 +4,4 @@ class Catalog < ActiveRecord::Base
   validates_length_of :art, :within => 1..10, :message =>"Данные введены неверно!"
   validates_length_of :name, :within => 1..50, :message =>"Данные введены неверно!"
 
-  
 end
